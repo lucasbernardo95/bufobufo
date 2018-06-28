bc9009ab04fe55cb1844f1bc4bd1581a98bc3032
Lucas <lucasbernardo95@gmail.com>
2018-06-28 15:39:12 -0300

    Inserção do potenciômetro
    Potenciômetro inserido para regular a sensibilidade do piezo.

--- bufobufo.ino
+++ bufobufo.ino
@@ -1,26 +1,29 @@
-#include <Thread.h>
-#include <ThreadController.h>
+//#include <Thread.h>
+//#include <ThreadController.h>
 #define input A0
+#define potenc A1
+
 int ledMax = 0;
 int sorteio = 4;
 unsigned long currentMillis = 0;
 long previousMillis  =     0;   
 long timeModoEspera  =    5000; 
+int valPot = 0;
 
-Thread threadMain;
-Thread threadEspera;
+//Thread threadMain;
+//Thread threadEspera;
+int leitura = 0;
 
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(9600);
-pinMode(3,INPUT);
-
-for(int i=4;i<14;i++){
-      pinMode(i,OUTPUT);
-      
-  }
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+    pinMode(3,INPUT);
+  
+    for(int i=4;i<14;i++){
+        pinMode(i,OUTPUT);  
+    }
 }
-int leitura = 0;
+
 void loop() {
   // put your main code here, to run repeatedly:
   //currentMillis = millis();
@@ -29,56 +32,65 @@ void loop() {
         modoEspera();  
  }*/
 
-if(digitalRead(3)){
-  modoEspera();
-  
- }{
-  
-leitura = analogRead(input);
-leitura = map(leitura,0,1023,0,1000)*2;
+    if(digitalRead(3)){
+      modoEspera();
+      
+    }{
+        valPot = map(analogRead(potenc),0,1023,0,255);
+        
+        leitura = analogRead(input);
 
+//        leitura = map(leitura,0,1023,0,1000 );//
+        leitura = map(leitura,0,1023,0,1023 - ((valPot/100) * 20));
 
-if(leitura > 50){
-  Serial.println(leitura);
-  if(leitura > 10 && leitura <= 100){
-    ledMax = 4;
-  }else if(leitura > 100 && leitura <= 200){
-    ledMax = 5;
-  }else if(leitura > 200 && leitura <= 300){
-    ledMax = 6;
-  }else if(leitura > 300 && leitura <= 400){
-    ledMax = 7;
-  }else if(leitura > 400 && leitura <= 500){
-    ledMax = 8;
-  }else if(leitura > 500 && leitura <= 600){
-    ledMax = 9;
-  }else if(leitura > 600 && leitura <= 700){
-    ledMax = 10;
-  }else if(leitura > 700 && leitura <= 800){
-    ledMax = 11;
-  }else if(leitura > 800 && leitura <= 900){
-    ledMax = 12;
-  }else if(leitura > 900 ){
-    ledMax = 13;
-  }
-  
-  for(int i=4;i <= ledMax;i++){
-      digitalWrite(i,HIGH);
-      delay(100000/leitura);
-  }
-  for(int i=ledMax;i >= 4;i--){
-      digitalWrite(i,LOW);
-      delay(100000/leitura);
-  }
-  if(ledMax == 11 || ledMax == 12 || ledMax == 13){
-     ganhou();
-     }
-
-  }
-  
-
- }
-}
+//        Serial.print("Leitura: ");
+//        Serial.print(leitura);
+//        Serial.print("   Potenciometro: ");
+//        Serial.print(valPot);
+//        Serial.print("   subtracao: ");
+//        Serial.print(((valPot/100) * 20));
+//        Serial.println("");
+        if(leitura > 50){
+              Serial.println(leitura);
+              if(leitura > 10 && leitura <= 50){
+                  ledMax = 4;
+              }else if(leitura > 50 && leitura <= 100){
+                  ledMax = 5;
+              }else if(leitura > 100 && leitura <= 170){
+                  ledMax = 6;
+              }else if(leitura > 170 && leitura <= 320){
+                  ledMax = 7;
+              }else if(leitura > 320 && leitura <= 500){
+                  ledMax = 8;
+              }else if(leitura > 500 && leitura <= 600){
+                  ledMax = 9;
+              }else if(leitura > 600 && leitura <= 700){
+                  ledMax = 10;
+              }else if(leitura > 700 && leitura <= 800){     
+                  ledMax = 11;
+              }else if(leitura > 800 && leitura <= 1000){
+                  ledMax = 12;
+              }else if(leitura > 1000 ){
+                  ledMax = 13;
+              }
+              
+              for(int i=4;i <= ledMax;i++){
+                  digitalWrite(i,HIGH);
+                  delay(100000/leitura);
+              }
+              for(int i=ledMax;i >= 4;i--){
+                  digitalWrite(i,LOW);
+                  delay(100000/leitura);
+              }
+              if(ledMax == 11 || ledMax == 12 || ledMax == 13){
+                 ganhou();
+                 }
+            
+              }
+          
+        
+         }
+    }
 
 
 
@@ -102,7 +114,6 @@ void ganhou(){
                 digitalWrite(i,LOW);
                 delay(10);
             }    
-     
      }
   
   }
